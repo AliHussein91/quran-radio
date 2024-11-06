08273ce6a3e2dc1448dfff41f7e712709030c24d
Ali Hussein <ali.hussein@live.com>
2024-11-06 13:37:31 +0200

    Player style updated

---
+++ main.js
@@ -0,0 +1,28 @@
+const body = document.querySelector('body')
+
+const player = document.querySelector('.audio-player');
+player.autoplay = false;
+
+const play = document.querySelector('.play');
+const pause = document.querySelector('.pause');
+
+// let quantity = 30
+// for (var i = 1; i <= quantity; i++){
+//     let div = document.createElement('div')
+//     div.classList.add('firefly')
+//     body.appendChild(div)
+// }
+  
+
+play.addEventListener('click', () => {
+	player.play();
+    pause.disabled = false
+    play.disabled = true
+});
+
+pause.addEventListener('click', () => {
+    player.pause();
+    pause.disabled = true
+    play.disabled = false
+
+});
