3ed75252885be68f9f2ae7fae070c9b780f489bb
Ali Hussein <ali.hussein@live.com>
2025-08-22 00:57:09 +0300

    Updated https links

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,22 @@
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("app-cache").then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/styles.css",
-        "/app.js",
-        "/icons/icon-192x192.png",
-        "/icons/icon-512x512.png"
-      ]);
-    })
-  );
+self.addEventListener('install', (event) => {
+	event.waitUntil(
+		caches.open('app-cache').then((cache) => {
+			return cache.addAll([
+				'/',
+				'/index.html',
+				'/main.css',
+				'/main.js',
+				'/icons/manifest-icon-192.maskable.png',
+				'/icons/manifest-icon-512.maskable.png',
+			]);
+		})
+	);
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', (event) => {
+	event.respondWith(
+		caches.match(event.request).then((response) => {
+			return response || fetch(event.request);
+		})
+	);
 });
