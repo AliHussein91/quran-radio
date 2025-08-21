55a0653c6070821937917e9e5be0453239321e41
Ali Hussein <ali.hussein@live.com>
2025-08-21 21:50:00 +0300

    Added PWA support

--- main.js
+++ main.js
@@ -26,3 +26,37 @@ pause.addEventListener('click', () => {
     play.disabled = false
 
 });
+
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("app-cache").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/main.css",
+        "/main.js",
+        "/icons/icon-192x192.png",
+        "/icons/icon-512x512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then(reg => console.log("Service Worker registered:", reg))
+      .catch(err => console.log("Service Worker failed:", err));
+  });
+}
